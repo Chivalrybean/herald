73e5fcad7973fc5dae1ee99b4459a1ad3fa96f59
Lowmaster <Justin.Lowmaster@flir.com>
2020-10-26 15:15:24 -0700

    new functions for data input.

--- herald.py
+++ herald.py
@@ -18,6 +18,23 @@ class Server:
 
 
 class Channel_settings:
-    def __init__(self, channel_id, channel_emoji):
+    def __init__(self, channel_id, channel_emoji=None, control_channel=False):
         self.channel_id = channel_id
         self.channel_id_emoji = channel_emoji
+        self.control_channel = control_channel
+
+
+def set_emoji(channel_id, emoji):
+    """Choose an emoji that will send message to specified channel when message is reacted to with that emoji
+    Arguments: 
+    channel ID(int) - the Discord ID for the channel
+    emoji(string) - The emoji reaction to trigger sending the message to the channel
+    """
+    new_setting = Channel_settings(channel_id, emoji)
+    return new_setting
+
+
+def set_control(channel_id):
+    """Select a channel and set it to the channel the bot will send reacted messages from."""
+    new_setting = Channel_settings(channel_id, None, True)
+    return new_setting
